f9dc7ae57c276a55c527de0b293f391e46237d6d
dangthai121200 <dangthai121200@gmail.com>
2022-04-01 20:55:39 +0700

    first commit

---
+++ src/test/java/herokuapp/com/RealEstateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package herokuapp.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
