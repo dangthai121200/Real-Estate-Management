86798a8eb1931bc13652c7346a374c95b4738121
dangthai121200 <dangthai121200@gmail.com>
2022-04-12 19:36:32 +0700

    fix error cross

--- src/main/java/com/herokuapp/realestatebk/RealEstateManagementApplication.java
+++ src/main/java/com/herokuapp/realestatebk/RealEstateManagementApplication.java
@@ -2,7 +2,11 @@ package com.herokuapp.realestatebk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,6 +15,16 @@ public class RealEstateManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealEstateManagementApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods(CorsConfiguration.ALL);
+			}
+		};
 	}
 
 }
