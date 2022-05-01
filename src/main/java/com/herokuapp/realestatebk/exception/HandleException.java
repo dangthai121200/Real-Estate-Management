2c839bc3fce18589be5f321ab45bb5d345209044
dangthai121200 <dangthai121200@gmail.com>
2022-05-01 19:09:27 +0700

    handle unique

--- src/main/java/com/herokuapp/realestatebk/exception/HandleException.java
+++ src/main/java/com/herokuapp/realestatebk/exception/HandleException.java
@@ -1,16 +1,41 @@
 package com.herokuapp.realestatebk.exception;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.herokuapp.realestatebk.util.UniqueDatabase;
 
 @ControllerAdvice
 public class HandleException {
 
 	@ExceptionHandler(RealEsateException.class)
-	public ResponseEntity<String> handleException(Exception ex) {
+	public ResponseEntity<String> handleRealEsateException(Exception ex) {
 		ex.printStackTrace();
 		return ResponseEntity.badRequest().body(ex.getMessage());
+	}
+
+	@ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+	public ResponseEntity<String> SQLIntegrityConstraintViolationException(
+			SQLIntegrityConstraintViolationException ex) {
+		ex.printStackTrace();
+		String message = "";
+		if (ex.getMessage().contains(UniqueDatabase.cmnd_UNIQUE)) {
+			message = "Chứng minh nhân dân đã tồn tại";
+		} else if (ex.getMessage().contains(UniqueDatabase.sodienthoai_UNIQUE)) {
+			message = "Số điện thoại đã tồn tại";
+		} else if (ex.getMessage().contains(UniqueDatabase.masoqsdd_UNIQUE)) {
+			message = "Quyền sử dụng đất đã tồn tại đã tồn tại";
+		} else if (ex.getMessage().contains(UniqueDatabase.taikhoan_UNIQUE)) {
+			message = "Username đã tồn tại đã tồn tại";
+		} else if (ex.getMessage().contains(UniqueDatabase.email_UNIQUE)) {
+			message = "Email đã tồn tại đã tồn tại";
+		} else {
+			message = "Có lỗi xảy ra vui lòng thử lại";
+		}
+		return ResponseEntity.badRequest().body(message);
 	}
 
 }
